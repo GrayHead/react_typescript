5570cf37577465c2cebd1897610947878d65b59a
GrayHead <ss.zhuravlov@gmail.com>
2024-04-21 18:04:24 +0300

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-import React, {FC} from 'react';
+import React, {FC, useReducer} from 'react';
 import './App.css';
 
+
+const reducerActions = (state: number, action: { type: string, payload: number }) => {
+    switch (action.type) {
+        case 'add':
+            return state + action.payload;
+        case 'getBack':
+            return state - action.payload;
+        case 'reset':
+            return action.payload;
+    }
+    return state;
+
+}
+
 const App: FC = () => {
+
+    const [number, dispatch] = useReducer(reducerActions, 0);
+
+    const add = () => {
+        dispatch({type: 'add', payload: 10});
+    }
+
+    const getBack = () => {
+        dispatch({type: 'getBack', payload: 5});
+    }
+
+    const reset = () => {
+        dispatch({type: 'reset', payload: -100});
+    }
+
+
     return (
         <>
+            <h2>Reducer demo.current value : {number}</h2>
+            <button onClick={add}>add</button>
+            <button onClick={getBack}>get back</button>
+            <button onClick={reset}>reset</button>
+            <hr/>
+
 
         </>
 
