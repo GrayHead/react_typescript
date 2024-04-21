f15c337acfab9e903f99b6ee55d2ffc61c2eef81
GrayHead <ss.zhuravlov@gmail.com>
2024-04-21 17:52:15 +0300

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
 
     let [x, setX] = useState<number>(0);
 
+
+    let y = useRef(0);
+
     useEffect(() => {
         console.log('work');
 
-        return ()=> {
+        return () => {
             console.log('!!!');
         }
     }, [x]);
@@ -17,10 +20,16 @@ const App: FC = () => {
         <>
             <button onClick={() => {
                 setX(prevState => ++prevState);
-            }}>
-                change
+            }}>change x
             </button>
 
+            <button onClick={() => {
+
+                y.current++;
+
+                console.log(y);
+            }}>change y
+            </button>
         </>
 
     );
