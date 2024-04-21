50d4858bd29ac170bb844ae92175655ac248019e
GrayHead <ss.zhuravlov@gmail.com>
2024-04-21 17:38:06 +0300

    useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
+    let [number, setNumber] = useState<number>(0);
+
+    // let number = 0;
+
+    const increment = () => {
+        setNumber(prevState => prevState + 1);
+    }
     return (
         <>
-
+            <h2>{number}</h2>
+            <button onClick={increment}> increment</button>
         </>
 
     );
