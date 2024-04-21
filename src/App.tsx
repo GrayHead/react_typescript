a01d301d9abb3b169ff1682a4cf355bc3dd8b389
GrayHead <ss.zhuravlov@gmail.com>
2024-04-21 17:45:28 +0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+    let [x, setX] = useState<number>(0);
+
+    useEffect(() => {
+        console.log('work');
+
+        return ()=> {
+            console.log('!!!');
+        }
+    }, [x]);
+
     return (
         <>
+            <button onClick={() => {
+                setX(prevState => ++prevState);
+            }}>
+                change
+            </button>
 
         </>
 
