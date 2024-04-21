f49fdb7f2cb81f753730a973e4cd09931d97f2cd
GrayHead <ss.zhuravlov@gmail.com>
2024-04-21 14:38:18 +0300

    comnponents with children

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,22 @@ const App: FC = () => {
             <Character name={'bart'}
                        image={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
             />
+
             <Character name={'homer'}
                        image={'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
-            />
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias aliquam assumenda, atque,
+                consectetur cumque enim eos maiores nemo nobis provident quae quidem ratione sint sit tempore temporibus
+                ullam?
+            </Character>
 
+            <Character name={'marge'}
+                       image={'https://i.pinimg.com/originals/af/76/7e/af767e6f4e6cb929c42c8c17bb0bae1e.png'}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias architecto, consequuntur cum
+                cupiditate eaque eligendi, est expedita fuga illo laboriosam nesciunt obcaecati odio rem reprehenderit
+                similique soluta vel, voluptatibus?
+
+            </Character>
 
         </>
 
--- src/components/character/Character.tsx
+++ src/components/character/Character.tsx
@@ -1,17 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, ReactNode} from 'react';
 
 
 interface IProps {
     name: string;
     image: string;
+    // children?: React.ReactNode;
 }
 
-const Character: FC<IProps> = ({name,image}) => {
+type PropsWithChildren<X> = X & { children?: ReactNode };
+
+const Character: FC<PropsWithChildren<IProps>> = ({name, image,children}) => {
 
     return (
         <div>
             <h2>{name}</h2>
             <img src={image} alt={name}/>
+            <p>{children}</p>
         </div>
     );
 };
