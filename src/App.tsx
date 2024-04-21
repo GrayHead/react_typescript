ce39f8fc0ffd0ad7e14c6482953f5438026c20c0
GrayHead <ss.zhuravlov@gmail.com>
2024-04-21 14:17:50 +0300

    comnponents conception

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, {FC} from 'react';
 import './App.css';
+import Character from "./components/character/Character";
 
 const App: FC = () => {
+
+
     return (
         <>
+            <Character/>
+            <Character/>
+
 
         </>
 
---
+++ src/components/character/Character.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+
+const Character: FC = () => {
+    return (
+        <div>
+            <h2>homer</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" alt=""/>
+        </div>
+    );
+};
+
+export default Character;
