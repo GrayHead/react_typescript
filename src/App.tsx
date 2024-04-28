6a1d924cc7894c4c6575802667376e984c5430ec
GrayHead <ss.zhuravlov@gmail.com>
2024-04-28 14:26:14 +0300

    uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from 'react';
 import './App.css';
+import FormComponent from "./components/FormComponent";
 
 const App: FC = () => {
     return (
         <>
-
+          <FormComponent/>
         </>
 
     );
---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FormComponent = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+            </form>
+        </div>
+    );
+};
+
+export default FormComponent;
