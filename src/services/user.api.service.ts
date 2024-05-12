f0ecfb767fffbf78f3fed20b6bd86e45f4edf4e4
GrayHead <ss.zhuravlov@gmail.com>
2024-05-12 21:07:05 +0300

    interceptors

--- src/services/user.api.service.ts
+++ src/services/user.api.service.ts
@@ -7,13 +7,21 @@ const axiosInstance = axios.create({
     headers: {'Content-Type': 'application/json'}
 });
 
+axiosInstance.interceptors.request.use(interceptedRequest => {
+    console.log(interceptedRequest);
+    // interceptedRequest.headers.login = 'foo';
+    // interceptedRequest.headers.password = 'bar';
+    interceptedRequest.headers.token = 'Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    return interceptedRequest;
+});
+
 const getUsers = (): Promise<AxiosResponse<IUser[]>> => {
     return axiosInstance.get('/users');
 }
-
 const getUser = async (id: number): Promise<AxiosResponse<IUser>> => {
     return await axiosInstance.get('/users/' + id);
 }
+
 export {
     getUsers,
     getUser
