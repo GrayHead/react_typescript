bf125676fef6235dc135dfb37abeeadbb9aff577
GrayHead <ss.zhuravlov@gmail.com>
2024-05-17 16:54:27 +0300

    add styles

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import styles from './HeaderComponent.module.css'
 
 const HeaderComponent = () => {
     return (
-        <div>
+        <div className={styles.xxx}>
             <span><NavLink to={'about'}>about</NavLink> </span>
 
             <span><NavLink to={'contacts'}>contacts</NavLink></span>
